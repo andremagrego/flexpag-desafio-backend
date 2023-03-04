13bc0087189516d8a48da2632f00accf7a1b9f4c
André Magrego <andremagrego@hotmail.com>
2023-03-04 19:06:51 -0300

    [FLEXPAG-000] Criação do arquivo repository

---
+++ src/main/java/com/flexpag/paymentscheduler/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.flexpag.paymentscheduler.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.flexpag.paymentscheduler.model.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+
+}
