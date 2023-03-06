27dd71032a6e565e7d55f8a36e51c209b5713b30
André Magrego <andremagrego@hotmail.com>
2023-03-05 21:21:36 -0300

    [FLEXPAG-000] Atualização do arquivo model

--- src/main/java/com/flexpag/paymentscheduler/model/Payment.java
+++ src/main/java/com/flexpag/paymentscheduler/model/Payment.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,15 +22,26 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idPayment;
 
-    @Column(name = "nome", length = 45)
-    private String nome;
+    @Column(name = "name", length = 45)
+    private String name;
 
-    @Column(name = "valor", length = 45)
-    private double valor;
+    @Column(name = "amount", length = 45)
+    private double amount;
 
-    @Column(name = "dataPagamento", length = 45)
-    private String dataPagamento;
+    @Column(name = "paymentDate", length = 45)
+    private String paymentDate;
+
+    @Column(name = "periodFrom", nullable = true, length = 45)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private String periodFrom;
+
+    @Column(name = "periodTo", nullable = true, length = 45)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private String periodTo;
+
+    @Column(name = "payDay", nullable = true, length = 45)
+    private String payDay;
 
     @Column(name = "status", length = 45)
-    private String status;
+    private boolean status;
 }
